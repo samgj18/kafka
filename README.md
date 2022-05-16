7ebc806c569d98dd80dad423b0d9bb1fe231ec9e
samgj18 <samgj_18@hotmail.com>
2022-05-16 20:05:29 +0100

    Adding consumer to read in batches

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Kafka Playground
 
 -   [x] Create a Kafka Producer
 -   [x] Create HTTP Server
--   [ ] Create a Kafka Consumer
--   [ ] Connect the Kafka Producer and Consumer to be triggered by a restful API (Http4s )
--   [ ] Make the Kafka Producer and Consumer work in the background with a single reusable instance of each
+-   [x] Create a Kafka Consumer
+-   [x] Connect the Kafka Producer and Consumer to be triggered by a restful API (Http4s )
+-   [x] Make the Kafka Producer and Consumer work in the background with a single reusable instance of each
+-   [ ] Make consumer lazy and read by batches
--- src/main/scala/com/experiments/sam/Main.scala
+++ src/main/scala/com/experiments/sam/Main.scala
@@ -1,5 +1,10 @@
 package com.experiments.sam
 
+import java.time.LocalDateTime
+import java.util.UUID
+
+import scala.util.control.NoStackTrace
+
 import cats.effect.kernel.{Async, Sync}
 import cats.effect.{IO, IOApp}
 import cats.syntax.all._
@@ -7,23 +12,26 @@ import com.comcast.ip4s._
 import com.experiments.sam.Kafka._
 import fs2.Stream
 import fs2.kafka._
+import io.circe.generic.semiauto._
+import io.circe.parser.decode
 import io.circe.syntax._
+import io.circe.{Decoder, KeyDecoder}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.ember.server._
 import org.http4s.implicits._
 import org.http4s.server.Server
+import io.circe.HCursor
 
-import java.time.LocalDateTime
-import java.util.UUID
 object Main extends IOApp.Simple {
 
   override def run: IO[Unit] = {
     val config = AppResources.make[IO]
     val program: Stream[IO, Server] =
       for {
-        producer <- KafkaProducer.stream(config.kafka)
-        kafka    <- Stream(Kafka.make(producer))
+        producer <- KafkaProducer.stream(config.producer)
+        consumer <- KafkaConsumer.stream(config.consumer).subscribeTo("topic")
+        kafka    <- Stream(Kafka.make(producer, consumer))
         server   <- MkHttpServer[IO].make(kafka).evalTap(_ => IO.never)
       } yield server
 
@@ -33,42 +41,96 @@ object Main extends IOApp.Simple {
 
 trait Kafka[F[_]] {
   def publish(msg: Message, topic: String): F[F[Result]]
+  def read: Stream[F, String]
 }
 
 object Kafka {
-  type Producer[F[_]]     = KafkaProducer[F, String, Message]
-  type Result             = ProducerResult[Unit, String, Message]
-  type KafkaService[F[_]] = ProducerSettings[F, String, Message]
+  type Producer[F[_]]             = KafkaProducer[F, String, Message]
+  type Consumer[F[_]]             = KafkaConsumer[F, String, Message]
+  type Result                     = ProducerResult[Unit, String, Message]
+  type KafkaProducerService[F[_]] = ProducerSettings[F, String, Message]
+  type KafkaConsumerService[F[_]] = ConsumerSettings[F, String, Message]
 
   case class Message(data: Map[String, String])
+
+  object Message {
+    implicit val decoder: Decoder[Message] = new Decoder[Message] {
+      final def apply(c: HCursor): Decoder.Result[Message] =
+        for {
+          name    <- c.downField("name").as[String]
+          source  <- c.downField("source").as[String]
+          email   <- c.downField("email").as[String]
+          uuid    <- c.downField("uuid").as[String]
+          surname <- c.downField("surname").as[String]
+        } yield new Message(
+          data = Map("name" -> name, "source" -> source, "email" -> email, "uuid" -> uuid, "surname" -> surname)
+        )
+    }
+  }
+
+  case class DecodingError(reason: String) extends NoStackTrace
+
+  final case class GenericMessage[K, V](data: Map[K, V])
+
+  object GenericMessage {
+    implicit def decoderForGenericMessage[K: KeyDecoder, V: Decoder]: Decoder[GenericMessage[K, V]] =
+      deriveDecoder[GenericMessage[K, V]]
+  }
+
   case class KafkaConfig(brokers: String, topic: String)
 
-  def make[F[_]](producer: Producer[F]) =
+  def make[F[_]: Async](producer: Producer[F], consumer: Consumer[F]) =
     new Kafka[F] {
       override def publish(msg: Message, topic: String): F[F[Result]] = {
         val key    = LocalDateTime.now.toString()
         val record = ProducerRecord(topic, key, msg)
         producer.produce(ProducerRecords.one(record))
       }
-    }
 
+      override def read =
+        consumer.records.mapAsync(2) { commitable =>
+          Async[F].delay(s"Processing record: ${commitable.record}")
+        }
+    }
 }
 
 sealed abstract case class AppResources[F[_]] private (
-  val kafka: KafkaService[F]
+  val producer: KafkaProducerService[F],
+  val consumer: KafkaConsumerService[F]
 )
 
 object AppResources {
-  def make[F[_]: Async]: AppResources[F] = {
-    def makeKafka(config: KafkaConfig): ProducerSettings[F, String, Message] =
-      ProducerSettings(
-        keySerializer = Serializer[F, String],
-        valueSerializer = Serializer.instance[F, Message] { (_, _, kafkaReq) =>
-          Sync[F].delay(kafkaReq.data.asJson.toString().getBytes("UTF-8"))
-        }
-      ).withBootstrapServers(config.brokers)
+  import Kafka._
 
-    new AppResources[F](makeKafka(KafkaConfig("localhost:9092", "topic"))) {}
+  def make[F[_]: Async]: AppResources[F] = {
+    def makeKafka(config: KafkaConfig) = {
+      lazy val kafkaProducer: ProducerSettings[F, String, Message] =
+        ProducerSettings(
+          keySerializer = Serializer[F, String],
+          valueSerializer = Serializer.instance[F, Message] { (_, _, kafkaReq) =>
+            Sync[F].delay(kafkaReq.data.asJson.toString().getBytes("UTF-8"))
+          }
+        ).withBootstrapServers(config.brokers)
+      lazy val kafkaConsumer: ConsumerSettings[F, String, Message] =
+        ConsumerSettings(
+          keyDeserializer = Deserializer[F, String],
+          valueDeserializer = Deserializer.instance[F, Message] { (_, _, kafkaReq) =>
+            decode[Message](new String(kafkaReq, "UTF-8")) match {
+              case Left(value)  => DecodingError(value.getMessage).raiseError[F, Message]
+              case Right(value) => value.pure[F]
+            }
+          }
+        ).withBootstrapServers(config.brokers)
+          .withGroupId("readers")
+          .withAutoOffsetReset(AutoOffsetReset.Earliest)
+
+      (kafkaProducer, kafkaConsumer)
+
+    }
+
+    val (producer, consumer) = makeKafka(KafkaConfig("localhost:9092", "topic"))
+
+    new AppResources[F](producer, consumer) {}
   }
 }
 
@@ -92,12 +154,16 @@ object MkHttpServer {
         )
       )
 
-    private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root =>
-      for {
-        id       <- genUUID
-        _        <- kafka.publish(mkMessage(id), "topic")
-        response <- Ok(id.toString)
-      } yield response
+    private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+      case GET -> Root =>
+        for {
+          id       <- genUUID
+          _        <- kafka.publish(mkMessage(id), "topic")
+          response <- Ok(id.toString)
+        } yield response
+
+      case GET -> Root / "read" =>
+        Ok(kafka.read)
     }
 
     val routes: HttpRoutes[F] = httpRoutes
